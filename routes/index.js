4413c7ee3a84bd1ad4dc4bf9d9e7a02a70af30d6
Chokchai213 <77828500+Chokchai213@users.noreply.github.com>
2023-02-14 00:44:43 +0700

    finished basic functions now can post new msg

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
+const { format } = require('morgan');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "GENERAL KENOBI",
+    user: "General Kenobi",
+    added: new Date()
+  },
+  {
+    text: "GENERAL GRIEVOUS",
+    user: "General Grievous",
+    added: new Date()
+  }
+];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board',messages });
 });
 
+router.get("/new",function(req,res,next){
+  res.render("message");
+});
+
+router.post("/new",function(req,res){
+  messages.push({
+    text: req.body.messages,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
+})
 module.exports = router;
